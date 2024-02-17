d58b8be179069d9c3fd5400731574a5f9a1b4e2a
rus7hex <dick525724@gmail.com>
2024-02-17 10:45:57 +0800

    burn token

--- ignition/modules/burn.js
+++ ignition/modules/burn.js
@@ -3,7 +3,14 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 module.exports = {
     default: buildModule(`sx${Date.now()}`, (m) => {
         const sbt = m.contractAt("ERC721ASoulBound", process.env.CONTRACT_ADDRESS);
-        m.call(sbt, "setIssuer", [process.env.ISSUER_ADDRESS]);
+        m.call(sbt, "burn", [0],
+            {
+                from: {
+                    type: "ACCOUNT",
+                    accountIndex: 1,
+                }
+            }
+        );
 
         return { sbt };
     })
--- ignition/modules/issue.js
+++ ignition/modules/issue.js
@@ -1,7 +1,7 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = {
-    default: buildModule("SecuX_Identity", (m) => {
+    default: buildModule(`sx${Date.now()}`, (m) => {
         const sbt = m.contractAt("ERC721ASoulBound", process.env.CONTRACT_ADDRESS);
         m.call(sbt, "issue", [process.env.ISSUER_ADDRESS],
             {
